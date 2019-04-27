007a8943b911a3e873633ab0677b68872bc3dcbd
curoky <cccuroky@gmail.com>
2019-04-27 14:55:26 +0800

    [stl]: example for language namespace

---
+++ dumbo/standard/language/namespaces/inline_namespace.cc
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2020 curoky(cccuroky@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ----------------------------------------------------------------------------
+ * @file: inline_namespace.cc
+ * ----------------------------------------------------------------------------
+ */
+
+#include <catch2/catch.hpp>
+
+namespace std {
+
+namespace c03 {
+int f() { return 0; }
+}  // namespace c03
+namespace c11 {
+int f() { return 1; }
+}  // namespace c11
+inline namespace c14 {
+int f() { return 2; }
+}  // namespace c14
+
+}  // namespace std
+
+TEST_CASE("[InlineNamespace]: ") {
+  REQUIRE(std::c03::f() == 0);
+  REQUIRE(std::c11::f() == 1);
+  REQUIRE(std::c14::f() == 2);
+  REQUIRE(std::f() == 2);
+}
---
+++ dumbo/standard/language/namespaces/unnamed_namespace.cc
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2020 curoky(cccuroky@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ----------------------------------------------------------------------------
+ * @file: unnamed_namespace.cc
+ * ----------------------------------------------------------------------------
+ */
+
+#include <catch2/catch.hpp>  // for AssertionHandler, SourceLineInfo, StringRef, operator""_catch_sr, REQUIRE, TEST_CASE
+
+#include <string>  // for operator==, string, allocator, basic_string
+
+namespace {
+std::string echo() { return "echo"; }
+}  // namespace
+
+TEST_CASE("[UnnamedNamespace]: simple test") { REQUIRE(echo() == "echo"); }
