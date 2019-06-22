a89c4cf4bcceb3e5875ffc5a873aa282e1e984e5
curoky <cccuroky@gmail.com>
2019-06-22 17:24:15 +0800

    [folly-fibers]: add example

---
+++ dumbo/scaffold/coroutine/folly-fibers/fibers.cc
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2020 curoky(cccuroky@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ----------------------------------------------------------------------------
+ * @file: fibers.cc
+ * ----------------------------------------------------------------------------
+ */
+
+#include <catch2/catch.hpp>
+#include <folly/fibers/Baton.h>
+#include <folly/fibers/Fiber.h>
+#include <folly/fibers/FiberManager.h>
+#include <folly/fibers/FiberManagerMap.h>
+#include <folly/io/async/EventBase.h>
+
+#include <iostream>
+
+TEST_CASE("[Fibers]: basic usage") {
+  folly::EventBase evb;
+  auto& fiberManager = folly::fibers::getFiberManager(evb);
+  folly::fibers::Baton baton;
+  fiberManager.addTask([&]() {
+    std::cout << "Task 1: start" << std::endl;
+    baton.wait();
+    std::cout << "Task 1: after baton.wait()" << std::endl;
+  });
+
+  fiberManager.addTask([&]() {
+    std::cout << "Task 2: start" << std::endl;
+    baton.post();
+    std::cout << "Task 2: after baton.post()" << std::endl;
+  });
+
+  evb.loop();
+}
