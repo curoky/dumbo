32aa0e8faf5b830d929b3ac812a446164344c27f
curoky <cccuroky@gmail.com>
2019-06-29 17:35:06 +0800

    [folly]: example for baton

---
+++ dumbo/scaffold/folly/sync/baton.cc
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2020 curoky(cccuroky@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <catch2/catch.hpp>
+#include <folly/synchronization/Baton.h>
+
+#include <thread>
+
+TEST_CASE("[Baton]: basic usage") {
+  folly::Baton b;
+
+  std::thread th([&b]() { b.wait(); });
+  b.post();
+
+  th.join();
+}
