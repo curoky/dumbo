b35a612a2080ee7d750a738c81c9fda14e543fbc
curoky <cccuroky@gmail.com>
2019-06-30 22:17:49 +0800

    [json]: update nlohmann json example

--- dumbo/scaffold/json/nlohmann-json.cc
+++ dumbo/scaffold/json/nlohmann-json.cc
@@ -17,10 +17,14 @@
 
 #include <catch2/catch.hpp>  // for AssertionHandler, operator""_catch_sr, SourceLineInfo, StringRef, REQUIRE, REQUIRE_FALSE, TEST_CASE
 #include <nlohmann/json.hpp>  // for basic_json, operator==, basic_json<>::object_t, operator<<, json
+#include <tbb/concurrent_unordered_map.h>
+#include <tbb/concurrent_unordered_set.h>
+#include <tbb/concurrent_vector.h>  // for concurrent_vector
 
 #include <initializer_list>  // for initializer_list
 #include <stdexcept>         // for out_of_range
-#include <vector>            // for vector, allocator
+#include <unordered_map>
+#include <vector>  // for vector, allocator
 
 #include "dumbo/scaffold/json/json_data.h"  // for json_data
 
@@ -68,14 +72,54 @@ TEST_CASE("[Nlohmann-json]: parse json") {
   REQUIRE(key_list_mix.is_array());
   REQUIRE(key_list_mix.at(0) == "1");
   REQUIRE(key_list_mix.at(1) == 2);
+  {
+    auto& key_map_eq = params["key_map_eq"];
+    REQUIRE(key_map_eq.is_object());
+    REQUIRE(key_map_eq.count("kk1") == 1);
+    REQUIRE(key_map_eq["kk2"] == "2");
 
-  auto& key_map_eq = params["key_map_eq"];
-  REQUIRE(key_map_eq.is_object());
-  REQUIRE(key_map_eq.count("kk1") == 1);
-  REQUIRE(key_map_eq["kk2"] == "2");
+    std::unordered_map<std::string, std::string> string_map =
+        key_map_eq.get<std::unordered_map<std::string, std::string>>();
+    REQUIRE(string_map.size() == 2);
+  }
+  {
+    auto& key_map_mix = params["key_map_mix"];
+    REQUIRE(key_map_mix.is_object());
+    REQUIRE(key_map_mix.at("kk1") == 1);
+    REQUIRE(key_map_mix.at("kk3").at(2) == 3);
 
-  auto& key_map_mix = params["key_map_mix"];
-  REQUIRE(key_map_mix.is_object());
-  REQUIRE(key_map_mix.at("kk1") == 1);
-  REQUIRE(key_map_mix.at("kk3").at(2) == 3);
+    bool have_except = false;
+    try {
+      std::unordered_map<std::string, std::string> string_map =
+          key_map_mix.get<std::unordered_map<std::string, std::string>>();
+      REQUIRE(string_map.size() == 3);
+    } catch (json::type_error& e) {
+      have_except = true;
+      REQUIRE(e.what() ==
+              std::string("[json.exception.type_error.302] type must be string, but is number"));
+    }
+    REQUIRE(have_except == true);
+  }
+}
+
+namespace tbb {
+void to_json(json& j, const concurrent_unordered_set<std::string>& p) {}
+
+void from_json(const json& j, concurrent_unordered_set<std::string>& p) {}
+}  // namespace tbb
+
+TEST_CASE("[Nlohmann-json]: tbb to/from json") {
+  json j;
+  {
+    tbb::concurrent_vector<int> vec;
+    j["vec"] = vec;
+  }
+  {
+    tbb::concurrent_unordered_set<std::string> set;
+    j["set"] = set;
+  }
+  {
+    tbb::concurrent_unordered_map<int, std::string> map;
+    j["map"] = map;
+  }
 }
